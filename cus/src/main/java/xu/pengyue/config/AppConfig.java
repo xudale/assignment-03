84c98b12b975c1aa9ed0def5170a8a6d84cbc94f
xupengyue <xupengyuehalin@163.com>
2026-01-09 22:44:41 +0100

    Fix AppConfig duplicate bean registration

--- cus/src/main/java/xu/pengyue/config/AppConfig.java
+++ cus/src/main/java/xu/pengyue/config/AppConfig.java
@@ -2,9 +2,7 @@ package xu.pengyue.config;
 
 import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.context.annotation.Configuration;
 
-@Configuration
 @ConfigurationProperties(prefix = "app")
 @Data
 public class AppConfig {
